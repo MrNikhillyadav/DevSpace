6cfb7406cc9bec848b3ad7fff6c1b54f5892bad1
Nikhil Yadav <facts.foundr@gmail.com>
2025-02-20 23:50:35 +0530

    add dynamic authorId to publish-new-blog

--- app/publish-new-blog/action.ts
+++ app/publish-new-blog/action.ts
@@ -3,29 +3,36 @@
 import prisma from "@/lib/db";
 import { newBlogSchema } from "@/lib/schema";
 import { revalidatePath } from "next/cache";
+import { getServerSession } from 'next-auth';
 
 
 export async function publishNewBlogPost(data: { title: string; content: string }) {
+  const session = await getServerSession();
+  const email = session?.user?.email;
 
   try {
-  
-    const validatedData = newBlogSchema.parse(data);
-
-    const post = await prisma.post.create({
-      data: { 
-        title: validatedData.title,
-        content: validatedData.content,
-        authorId: "cm7ae0rbf0006v0g8cu1u02wx",
-        published: true
+      const user = await prisma.user.findFirst({
+        where: {
+          email,
+        },
+  });
+      if (!user) {
+        return { message: 'User not found' };
       }
-    });
+      const validatedData = newBlogSchema.parse(data);
+      await prisma.post.create({
+        data: { 
+          title: validatedData.title,
+          content: validatedData.content,
+          authorId: user.id,
+          published: true
+        }
+      });
 
-    
-    revalidatePath('/feed');
-    
-    
+      revalidatePath('/feed'); 
+
   } catch (error) {
-    console.error("Error publishing post:", error);
+      console.error("Error publishing post:", error);
     if (error instanceof Error) {
       return { error: error.message };
     }
